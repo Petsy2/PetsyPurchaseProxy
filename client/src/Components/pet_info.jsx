5d06777be513e5e1f407aa8dd987fe5266f097ff
Michael Nutt <mnutt346@gmail.com>
2019-01-05 11:13:29 -0600

    Attempt nesting purchase component in grid.

--- client/src/Components/pet_info.jsx
+++ client/src/Components/pet_info.jsx
@@ -52,7 +52,7 @@ class PetInfo extends React.Component {
           className="pet-image-container"
           image_url={this.state.image_url}
         />
-        <div />
+        <div id="root" />
         <div />
         <div />
         <PetDescription description={this.state.description} />
