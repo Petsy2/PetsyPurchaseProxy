d84aaa8d9deeb26b811326688e54fbb39d125b42
Michael Nutt <mnutt346@gmail.com>
2019-01-07 15:14:52 -0600

    Correct inception bug.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => { console.log(`listening on port ${port}`) })
-
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
